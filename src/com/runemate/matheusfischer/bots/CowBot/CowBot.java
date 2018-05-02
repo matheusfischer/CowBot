36cfaf87288a9a8e793c56252b778e9c9651db89
matheusfischer <fischer.matheusf@gmail.com>
2018-05-02 00:13:12 -0300

    manifest

--- src/com/runemate/matheusfischer/bots/CowBot/CowBot.java
+++ src/com/runemate/matheusfischer/bots/CowBot/CowBot.java
@@ -1,6 +1,31 @@
 package com.runemate.matheusfischer.bots.CowBot;
 
-import com.runemate.game.api.script.framework.task.TaskBot;
+import com.runemate.game.api.hybrid.location.Coordinate;
+import com.runemate.game.api.hybrid.location.navigation.cognizant.RegionPath;
+import com.runemate.game.api.hybrid.region.Players;
+import com.runemate.game.api.script.Execution;
+import com.runemate.game.api.script.framework.LoopingBot;
 
-public class CowBot extends TaskBot {
+public class CowBot extends LoopingBot {
+    public static final int GLOBAL_TIMEOUT = 5000;
+
+    public boolean goToPosition(Coordinate position) {
+        RegionPath path = RegionPath.buildTo(position);
+
+        if (path == null) {
+            return false;
+        }
+
+        path.step();
+        return Execution.delayUntil(() -> Players.getLocal().distanceTo(position) < 2, GLOBAL_TIMEOUT);
+    }
+
+    @Override
+    public void onStart(String... args)
+    {
+    }
+
+    @Override
+    public void onLoop() {
+    }
 }
